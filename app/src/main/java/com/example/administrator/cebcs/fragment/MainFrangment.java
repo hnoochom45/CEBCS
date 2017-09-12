b5a91866456140a912a606371d37db4059c7e362
hnoo <hnoona46335@gmail.com>
2017-09-12 10:43:11 +0700

    step2

--- app/src/main/java/com/example/administrator/cebcs/fragment/MainFrangment.java
+++ app/src/main/java/com/example/administrator/cebcs/fragment/MainFrangment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.administrator.cebcs.R;
 
@@ -19,5 +20,31 @@ public class MainFrangment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main,container,false);
         return view;
+    }   //onCreatView
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Register Controller
+        registerController();
+
+
+    }   //onActivityCreat
+
+    private void registerController() {
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentFragmentMain,new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+            }
+        });
     }
 }   //
